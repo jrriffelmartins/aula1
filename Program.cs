43d6d63ca890cd44f9aa65ca958801c9f27336d1
Júnior LuÍz Riffel Martins <jm0097667@setrem.com.br>
2021-09-04 09:19:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<<<>>>>");
+            Console.WriteLine("Calcular area do terreno");
+            Console.WriteLine("<<<>>>>");
+
+            Console.WriteLine("Informe a largura do terreno");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a comprimento do terreno");
+            int comprimento = Convert.ToInt32(Console.ReadLine());
+
+            calcularArea(largura, comprimento);
+
+           
+        }
+
+        static void calcularArea(int largura,int comprimento)
+
+        {
+            int area = largura * comprimento;
+            Console.WriteLine($@"Area={area}m²");
+        }
+
+    }
+}
